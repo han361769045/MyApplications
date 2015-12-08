00f13d42ed92b17f0738fa32707322a761b38d3e
ljq <ljq@zczczy.com>
2015-12-08 20:14:20 +0800

    更新中间按钮

--- app/src/main/java/com/luleo/myapplications/broadcast/MyBroadcast.java
+++ app/src/main/java/com/luleo/myapplications/broadcast/MyBroadcast.java
@@ -3,12 +3,38 @@ package com.luleo.myapplications.broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import org.androidannotations.annotations.EReceiver;
+import org.androidannotations.annotations.ReceiverAction;
+import org.androidannotations.annotations.SystemService;
+import org.androidannotations.api.BackgroundExecutor;
 
 /**
  * Created by leo on 2015/7/21.
  */
+
+@EReceiver
 public class MyBroadcast extends BroadcastReceiver {
 
+    @SystemService
+    ConnectivityManager manager;
+
+
+
+
+
+    @ReceiverAction("android.net.conn.CONNECTIVITY_CHANGE")
+    public void connectivity(){
+        NetworkInfo mobileInfo = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        NetworkInfo wifiInfo = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo activeInfo = manager.getActiveNetworkInfo();
+        if(activeInfo==null){
+            BackgroundExecutor.cancelAll("",true);
+        }
+
+    }
 
 
 
